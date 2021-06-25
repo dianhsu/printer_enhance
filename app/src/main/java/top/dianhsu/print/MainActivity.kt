3819797e7ac69d0f365fef9b9e67793de92f75b7
myhbaba <437942378@qq.com>
2021-06-25 21:30:31 +0800

    ui changed

--- app/src/main/java/top/dianhsu/print/MainActivity.kt
+++ app/src/main/java/top/dianhsu/print/MainActivity.kt
@@ -1,53 +1,130 @@
 package top.dianhsu.print
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.os.Message
 import android.util.Log
+import android.view.View
 import android.widget.*
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import de.gmuth.ipp.cups.CupsClient
 import java.net.URI
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var printerSpinner: Spinner
+    private lateinit var ivPrinter:ImageView
+    private lateinit var ivPageSelect:ImageView
+    private lateinit var ivDoubleSide:ImageView
+    private lateinit var ivFileAdd:ImageView
+    private lateinit var ivFilePreview:ImageView
+    private lateinit var selectPdfBtn: ConstraintLayout
+    private lateinit var previewPdfBtn:ConstraintLayout
+    private lateinit var printPdfBtn: Button
     private lateinit var printerSpinnerAdapter: ArrayAdapter<String>
     private lateinit var printerData: ArrayList<String>
     private lateinit var printerUri: ArrayList<String>
+    private var fileName:String?=null
     private var pdfUri: Uri? = null
     private lateinit var pagesEditText: EditText
+    private var _handler: Handler?=null
+    private val mHandler get() = _handler!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_beta)
-        printerSpinner = findViewById(R.id.spinner)
+        initData()
+        initView()
+        initListener()
+        initPrinter()
+        initHandler()
+    }
+
+    private fun initData() {
         printerData = ArrayList()
         printerUri = ArrayList()
+    }
+
+    private fun initHandler() {
+        _handler=object : Handler(mainLooper) {
+            override fun handleMessage(msg: Message) {
+                super.handleMessage(msg)
+                when (msg.what) {
+                    0 -> {
+                        //完成主界面更新,拿到数据
+                        printerData.clear()
+                        printerUri.clear()
+                        val printer = msg.obj as ArrayList<*>?
+                        printer?.forEach {
+                            val nex = it as Printer.PrinterInfo
+                            Log.d("Find Printer", "${nex.name}, ${nex.uri}")
+                            printerData.add(nex.name.toString())
+                            printerUri.add(nex.uri.toString())
+                        }
+                        printerSpinnerAdapter.notifyDataSetChanged()
+                    }
+                    else -> {
+                    }
+                }
+            }
+        }
+    }
+
+    private fun initPrinter() {
+        printerDiscovery()
+    }
+
+    private fun initView() {
+        printerSpinner = findViewById(R.id.spinner)
         pagesEditText = findViewById(R.id.pages_edit_text)
         printerSpinnerAdapter =
             ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, printerData)
         printerSpinner.adapter = printerSpinnerAdapter
-        val selectPdfBtn: LinearLayout = findViewById(R.id.select_pdf_btn)
-        val printPdfBtn: Button = findViewById(R.id.print_pdf_btn)
+        ivDoubleSide=findViewById(R.id.iv_double_sided)
+        ivFileAdd=findViewById(R.id.iv_file_add)
+        ivFilePreview=findViewById(R.id.iv_file_preview)
+        ivPageSelect=findViewById(R.id.iv_pages)
+        ivPrinter=findViewById(R.id.iv_printer)
+        selectPdfBtn=findViewById(R.id.select_pdf_btn)
+        previewPdfBtn=findViewById(R.id.preview_pdf_btn)
+        printPdfBtn=findViewById(R.id.print_pdf_btn)
+    }
+
+    private fun initListener() {
 
         selectPdfBtn.setOnClickListener {
-            content.launch(Intent(this,PdfPreviewActivity::class.java))
+            content.launch(Intent(this, PdfPreviewActivity::class.java).apply {
+                putExtra("isPreview",false)
+            })
         }
-
 
         printPdfBtn.setOnClickListener {
             //printDocument()
         }
-        printerDiscovery()
+
+        previewPdfBtn.setOnClickListener {
+            val intent=Intent(this,PdfPreviewActivity::class.java)
+            intent.putExtra("isPreview",true)
+            intent.data = pdfUri
+            intent.putExtra("fileName",fileName)
+            Log.e("MainActivity",intent.data.toString())
+            startActivity(intent)
+        }
     }
 
-    private val content=registerForActivityResult(ActivityResultContracts.StartActivityForResult()
+
+    private val content = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
     ) {
-        if (it.resultCode==111) pdfUri = it.data?.data
+        if (it.resultCode == 111){
+            pdfUri = it.data?.data
+            fileName=it.data?.getStringExtra("fileName")
+            Log.e("MainActivityReturn",fileName.toString())
+        }
+
     }
 
     private fun printDocument() {
@@ -74,28 +151,9 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    var mHandler: Handler = @SuppressLint("HandlerLeak")
-    object : Handler() {
-        override fun handleMessage(msg: Message) {
-            super.handleMessage(msg)
-            when (msg.what) {
-                0 -> {
-                    //完成主界面更新,拿到数据
-                    printerData.clear()
-                    printerUri.clear()
-                    val printer = msg.obj as ArrayList<*>?
-                    printer?.forEach {
-                        val nex = it as Printer.PrinterInfo
-                        Log.d("Find Printer", "${nex.name}, ${nex.uri}")
-                        printerData.add(nex.name.toString())
-                        printerUri.add(nex.uri.toString())
-                    }
-                    printerSpinnerAdapter.notifyDataSetChanged()
-                }
-                else -> {
-                }
-            }
-        }
+    override fun onDestroy() {
+        super.onDestroy()
+        mHandler.removeCallbacksAndMessages(null)
     }
 }
 
--- app/src/main/java/top/dianhsu/print/PdfPreviewActivity.kt
+++ app/src/main/java/top/dianhsu/print/PdfPreviewActivity.kt
@@ -2,23 +2,24 @@ package top.dianhsu.print
 
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.net.toUri
+import androidx.lifecycle.lifecycleScope
 import com.pdfview.PDFView
-import com.tom_roush.pdfbox.pdmodel.PDDocument
-import com.tom_roush.pdfbox.util.PDFBoxResourceLoader
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.File
 import java.util.*
 
 class PdfPreviewActivity : AppCompatActivity() {
     private lateinit var pdfView: PDFView
-    private var pdfUri: Uri? = null
-    private lateinit var tb_pdf:Toolbar
-
-
+    private lateinit var tbPdf: Toolbar
+    private lateinit var file: File
+    private var fileName: String? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,63 +27,79 @@ class PdfPreviewActivity : AppCompatActivity() {
         setContentView(R.layout.activity_pdf_preview)
         initView()
         initPdf()
-
-
     }
 
     private fun initPdf() {
-        getContent.launch("application/pdf")
+        if (!intent.getBooleanExtra("isPreview", false)) {
+            getContent.launch("application/pdf")
+        } else {
+            Log.e("PreviewFileName", intent.getStringExtra("fileName").toString())
+            val fileName = intent.getStringExtra("fileName")
+            fileName?.let {
+                showPdf(File(application.cacheDir,fileName))
+            }
+        }
+    }
+
+    private fun showPdf(uri: Uri) {
+        lifecycleScope.launch {
+            withContext(Dispatchers.IO) {
+                file = generatePdfFromUri(uri)
+            }
+            withContext(Dispatchers.Main) {
+                pdfView.fromFile(file).show()
+            }
+        }
+    }
+
+    private fun showPdf(file:File){
+        pdfView.fromFile(file).show()
     }
 
     private fun initView() {
         pdfView = findViewById(R.id.pdf_view)
-        tb_pdf=findViewById(R.id.tb_pdf)
-        tb_pdf.setNavigationOnClickListener {
+        tbPdf = findViewById(R.id.tb_pdf)
+        tbPdf.setNavigationOnClickListener {
             this.finish()
         }
     }
 
-
-    private var getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
-        if (uri != null) {
-            val outputDir = applicationContext.cacheDir
-            val outputFile: File =
-                File.createTempFile("${UUID.randomUUID()}", ".pdf", outputDir)
-            val ins = contentResolver.openInputStream(uri)
-            val buffer = ByteArray(1024)
-            ins.use { input ->
-                outputFile.outputStream().use { fileOut ->
-                    while (true) {
-                        val length = input?.read(buffer)
-                        if (length != null) {
-                            if (length <= 0) {
-                                break
-                            }
-                            fileOut.write(buffer, 0, length)
+    private fun generatePdfFromUri(uri: Uri): File {
+        val outputDir = applicationContext.cacheDir
+        val outputFile: File =
+            File.createTempFile("${UUID.randomUUID()}", ".pdf", outputDir)
+        val ins = contentResolver.openInputStream(uri)
+        val buffer = ByteArray(1024)
+        ins.use { input ->
+            outputFile.outputStream().use { fileOut ->
+                while (true) {
+                    val length = input?.read(buffer)
+                    if (length != null) {
+                        if (length <= 0) {
+                            break
                         }
+                        fileOut.write(buffer, 0, length)
                     }
-                    fileOut.flush()
                 }
+                fileOut.flush()
             }
-
-            pdfUri = outputFile.toUri()
-            pdfView.fromFile(outputFile).show()
-            PDFBoxResourceLoader.init(baseContext)
-            var doc = PDDocument.load(outputFile)
-            tb_pdf.title = "${doc.numberOfPages}"
-//            if (doc.numberOfPages % 2 != 0) {
-//                doc.addPage(PDPage())
-//            }
-//            doc.save(outputFile)
-            doc.close()
-            doc = PDDocument.load(outputFile)
         }
+
+        fileName=outputFile.name
+
+        setResult(111, Intent().apply {
+            putExtra("fileName",fileName)
+            data=uri
+        })
+
+        return outputFile
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        setResult(111, Intent().apply {
-            this.data = pdfUri
-        })
-    }
+
+    private var getContent =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+            if (uri != null) {
+                showPdf(uri)
+            }
+        }
 }
